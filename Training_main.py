2c30a336a44b6377fe462f48d7164415bda2ae9a
AliAshram <aliashram.pc@gmail.com>
2025-04-13 16:34:08 +0300

    remove working dir setting

--- Training_main.py
+++ Training_main.py
@@ -12,8 +12,6 @@ from Model import TrainModel
 from Visualization import Visualization
 from Utils import import_train_configuration, set_sumo, set_train_path
 
-print(os.getcwd())
-os.chdir("/home/ali/Documents/Ali/MY_Grad_Proj")
 if __name__ == "__main__":
 
     config = import_train_configuration(config_file='Training_settings.ini')
