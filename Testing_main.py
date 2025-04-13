58b4f415706270101855d6ee4de2b0db1a04c565
AliAshram <aliashram.pc@gmail.com>
2025-04-13 16:31:16 +0300

    first commit

---
+++ Testing_main.py
@@ -0,0 +1,56 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+from shutil import copyfile
+
+from Testing_simulation import Simulation
+from Generator import TrafficGenerator
+from Model import TestModel
+from Visualization import Visualization
+from Utils import import_test_configuration, set_sumo, set_test_path
+
+os.chdir("/home/ali/Documents/PyVenv/MY_Grad_Proj")
+
+if __name__ == "__main__":
+
+    config = import_test_configuration(config_file='Testing_settings.ini')
+    sumo_cmd = set_sumo(config['gui'], config['sumocfg_file_name'], config['max_steps'])
+    model_path, plot_path = set_test_path(config['models_path_name'], config['model_to_test'])
+
+    Model = TestModel(
+        input_dim=config['num_states'],
+        model_path=model_path
+    )
+
+    TrafficGen = TrafficGenerator(
+        config['max_steps'], 
+        config['n_cars_generated']
+    )
+
+    Visualization = Visualization(
+        plot_path, 
+        dpi=96
+    )
+        
+    Simulation = Simulation(
+        Model,
+        TrafficGen,
+        sumo_cmd,
+        config['max_steps'],
+        config['green_duration'],
+        config['yellow_duration'],
+        config['num_states'],
+        config['num_actions']
+    )
+
+    print('\n----- Test episode')
+    simulation_time = Simulation.run(config['episode_seed'])  # run the simulation
+    print('Simulation time:', simulation_time, 's')
+
+    print("----- Testing info saved at:", plot_path)
+
+    copyfile(src='Testing_settings.ini', dst=os.path.join(plot_path, 'Testing_settings.ini'))
+
+    Visualization.save_data_and_plot(data=Simulation.reward_episode, filename='reward', xlabel='Action step', ylabel='Reward')
+    Visualization.save_data_and_plot(data=Simulation.queue_length_episode, filename='queue', xlabel='Step', ylabel='Queue lenght (vehicles)')
---
+++ test.py
@@ -0,0 +1,3 @@
+import numpy as np
+states = np.zeros(80)
+print(states)
